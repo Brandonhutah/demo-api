fa769e9507a8fb2085cefaf718a8fe9a9793ca13
Brandon Horlacher <brandonh.utah@gmail.com>
2023-02-04 16:03:05 -0700

    allowing error requests to be returned.

--- src/main/java/com/brandonhorlacher/demoapi/security/WebSecurityConfiguration.java
+++ src/main/java/com/brandonhorlacher/demoapi/security/WebSecurityConfiguration.java
@@ -3,13 +3,11 @@ package com.brandonhorlacher.demoapi.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -24,7 +22,7 @@ public class WebSecurityConfiguration {
                 .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth**")
+                        .requestMatchers("/auth**", "/error")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
